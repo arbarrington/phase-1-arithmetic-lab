84ea892c66c1635e89de151986fd304d025af31c
Austin Barrington <arbarrington12@gmail.com>
2022-08-08 16:39:04 -0600

    commit tree cd34c71bb728a8256204629ae0982c318f80b74b

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
+let num1 = 31;
+let num2 = 2;
+const multiply = num1*num2;
+const random = Math.floor(Math.random() * 10) + 1;
+let num3 = 5;
+let num4 = 1;
+const mod = num3 - num4;
+const max = Math.max(num2,num3, 20);
 
-// Write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
